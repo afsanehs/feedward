792a17fe27b619ddf69fafcdeddb4cdb3cc49ff0
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-07 12:52:39 +0200

    Implement bootsttrap, navbar, footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def active_class(link_path)
+    current_page?(link_path) ? "active" : ""
+  end
+  def flash_class(level)
+    case level
+        when "notice"
+          return "alert alert-info"
+        when "success"
+          return "alert alert-success"
+        when "error" 
+          return "alert alert-danger"
+        when "warning" 
+          return "alert alert-warning"
+        when "alert"
+          return "alert alert-danger"
+    end
+  end
+
 end
