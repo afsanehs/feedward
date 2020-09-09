029dd8ff818c6c01e4714a30dd38adf874f1612b
donatellalnrs <donatella.gonzalezdelinares@essec.edu>
2020-09-09 17:37:43 +0200

    js for the pie chart

--- app/views/users/partials/chart-pie.js
+++ app/views/users/partials/chart-pie.js
@@ -1,4 +1,6 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
+console.log("coucou")
+
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
