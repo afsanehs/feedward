729b21d21ab90534c34bce2074e1a4f7ab435e31
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-10 18:40:13 +0200

    Fix bug color

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 // [...]
-import "../src/style.scss";
+import '../src/style.scss';
 
-require("jquery");
-require("bootstrap");
+require('jquery');
+require('bootstrap');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -12,6 +12,5 @@ require("bootstrap");
 // const imagePath = (name) => images(name, true)
 require('chartkick');
 require('chart.js');
-require('jquery');
 require('popper.js');
 require('admin2/sb-admin-2');
