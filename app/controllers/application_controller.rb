346583755a4e3a115178452b01dfe4cf63cbfd4f
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-10 15:58:45 +0200

    Redirect to dashboard after login and register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || dashboard_path
+  end
+  def after_sign_up_path_for(resource)
+    stored_location_for(resource) || dashboard_path
+  end
 end
