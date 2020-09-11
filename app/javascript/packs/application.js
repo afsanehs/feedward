0d33ccbfad49a35f9802a3c17354f7204584fa5d
Meyanis95 <yanis.meziane@essec.edu>
2020-09-11 17:43:27 +0200

    Add style.css for heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
-// [...]
+import '../src/style.scss';
 
-require("jquery");
-require("bootstrap");
+require('jquery');
+require('bootstrap');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -9,7 +9,7 @@ require("bootstrap");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-require("chartkick");
-require("chart.js");
-require("popper.js");
-require("admin2/sb-admin-2");
+require('chartkick');
+require('chart.js');
+require('popper.js');
+require('admin2/sb-admin-2');
