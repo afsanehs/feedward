4e6fc60a89bc41c6257a860d7d2080f72216cb2b
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-14 17:02:45 +0200

    Update carts user, table dashboard admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,11 +68,12 @@ class UsersController < ApplicationController
 
   def dashboard_admin
     if !current_user.is_site_admin && !current_user.is_company_admin
-        flash[:error] = "Vous n'avez pas de droit pour accéder cette page."
+        flash[:error] = "Vous n'avez pas de droit pour accéder à cette page."
         return redirect_to dashboard_path
     end
     @feedbacks = Feedback.all_company_fbs(current_user.company_id)
     @company = current_user.company
+    @company_users = User.where(company_id: @company.id)
 
 
     @score_global_average = Feedback.global_score(@company.id)
@@ -95,7 +96,7 @@ class UsersController < ApplicationController
     @score_workspace_average_evolution = (100*(@score_workspace_average - @score_workspace_average_yesterday) / @score_workspace_average_yesterday).round(2)
     @score_missions_average_evolution = (100*(@score_missions_average - @score_missions_average_yesterday) / @score_missions_average_yesterday).round(2)
 
-    #calculations for the pie Chart
+    #calculations for the pie Chart of today
     @grade_5 = (@feedbacks.where(score_global: 5).count + @feedbacks.where(score_workspace: 5).count + @feedbacks.where(score_missions: 5).count)
     @grade_4 = (@feedbacks.where(score_global: 4).count + @feedbacks.where(score_workspace: 4).count + @feedbacks.where(score_missions: 4).count)
     @grade_3 = (@feedbacks.where(score_global: 3).count + @feedbacks.where(score_workspace: 3).count + @feedbacks.where(score_missions: 3).count)
@@ -108,6 +109,66 @@ class UsersController < ApplicationController
     @all_grades.each do |score, _|
       @colors << @score_colors[score]
     end
+
+    #calculations for the pie Chart of yesterday
+    @grade_5_yesterday = (@feedbacks.where(score_global: 5).count + @feedbacks.where(score_workspace: 5).count + @feedbacks.where(score_missions: 5).count)
+    @grade_4_yesterday = (@feedbacks.where(score_global: 4).count + @feedbacks.where(score_workspace: 4).count + @feedbacks.where(score_missions: 4).count)
+    @grade_3_yesterday = (@feedbacks.where(score_global: 3).count + @feedbacks.where(score_workspace: 3).count + @feedbacks.where(score_missions: 3).count)
+    @grade_2_yesterday = (@feedbacks.where(score_global: 2).count + @feedbacks.where(score_workspace: 2).count + @feedbacks.where(score_missions: 2).count)
+    @grade_1_yesterday = (@feedbacks.where(score_global: 1).count + @feedbacks.where(score_workspace: 1).count + @feedbacks.where(score_missions: 1).count)
+    @grade_0_yesterday = (@feedbacks.where(score_global: 0).count + @feedbacks.where(score_workspace: 0).count + @feedbacks.where(score_missions: 0).count)
+    @score_colors = {"Note 5" => "#22347A", "Note 4" => "#6558F1", "Note 3" => "#B2ACFA", "Note 2" => "#CE885D", "Note 1" => "#DFB090", "Note 0" => "#F6E8DF"}
+    @all_grades_yesterday = {"Note 5" => @grade_5_yesterday, "Note 4" => @grade_4_yesterday, "Note 3" => @grade_3_yesterday, "Note 2" => @grade_2_yesterday, "Note 1" => @grade_1_yesterday, "Note 0" => @grade_0_yesterday}
+    @colors = []
+    @all_grades_yesterday.each do |score, _|
+      @colors << @score_colors[score]
+    end
+
+    # Data for line charts
+    @line_feedbacks=[]
+    hash= Hash.new
+    hash["name"] = "Moyenne de toutes les notes"
+    hash["data"] = @feedbacks.group_by_week(:created_at).average(:score_average)
+    @line_feedbacks.push(hash)
+
+    hash= Hash.new
+    hash["name"] = "L'entreprise en général"
+    hash["data"] = @feedbacks.group_by_week(:created_at).average(:score_global)
+    # hash["data"]=get_missing_day_for_linechart(origin_group)
+    @line_feedbacks.push(hash)
+    
+    hash= Hash.new
+    hash["name"] = "Le cadre de travail"
+    hash["data"] = @feedbacks.group_by_week(:created_at).average(:score_workspace)
+    @line_feedbacks.push(hash)
+    
+    hash= Hash.new
+    hash["name"] = "Les missions des salariés"
+    hash["data"]=@feedbacks.group_by_week(:created_at).average(:score_missions)
+    @line_feedbacks.push(hash)
+    @today=Date.today
+    @two_month_ago = @today - 8.week
+
+    #Data for warning texts
+    @warned_feedbacks = []
+    @feedbacks.each do |feedback|
+      if feedback.answer_final != "" && feedback.created_at >= (Time.now - 3.day)
+        @warned_feedbacks << feedback
+      end
+    end 
+    @warned_feedbacks = @warned_feedbacks.sort_by &:created_at
+    @warned_feedbacks.reverse!
+
+
+    #Data for alerts
+    @unsatisfied_users = []
+    @company_users.each do |user|
+      if Feedback.user_score(user.id) < 3
+        @unsatisfied_users << user
+      end
+    end 
+    @unsatisfied_users = @unsatisfied_users.sort_by{|u| Feedback.user_score(u.id)}
+
   end
 
 
@@ -128,4 +189,27 @@ class UsersController < ApplicationController
       :company_id
     )
   end
+
+  def get_first_created_day
+    day=Feedback.order(:created_at).first.created_at.to_date
+    return  day
+  end
+  def get_last_created_day
+    return Feedback.order(:created_at).last.created_at.to_date
+  end
+
+  def get_missing_day_for_linechart(group_by_day)
+    origin_group = group_by_day
+    start= get_first_created_day
+    last = get_last_created_day
+    while(start < last) do
+      if !origin_group.key?(start)
+        origin_group[start]=origin_group[start-1]
+      end
+      start += 1.day
+    end
+
+    return origin_group
+  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,10 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+
   end
   
   resources :feedbacks
-  resources :users, only: [:show]
 
 
 end
