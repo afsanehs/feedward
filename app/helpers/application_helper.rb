15230070a93c9d293a435dcfa6889145e1ca8f43
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-14 17:00:07 +0200

    Add method get_date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,6 +24,9 @@ module ApplicationHelper
   def get_time_verbose(time_utc)
     return l(time_utc, format: '%d/%m/%Y - à %k:%M')
   end
+  def get_date(time_utc)
+    return l(time_utc, format: '%d/%m/%Y')
+  end
 
   #----------------------------------#
   # Using devise form in another page
