3753cd3e76ddb119aa9ea06c45f8d2de4312b81f
donatellalnrs <donatella.gonzalezdelinares@essec.edu>
2020-09-16 16:55:49 +0200

    Resolved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_09_16_085026) do
-=======
-ActiveRecord::Schema.define(version: 2020_09_16_124309) do
->>>>>>> 6126bb66440946579c52d6961e33bd5622883dab
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
