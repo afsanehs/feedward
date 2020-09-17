ccd4b9e43a4ee6149c3abf6cc5ea92e59f415da0
afsanehs <afsaneh.samettehrani@essec.edu>
2020-09-17 14:26:27 +0100

    remove unneeded config
    i think...

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['MAILJET_LOGIN'],
   :password => ENV['MAILJET_PASSWORD'],
-  :domain => 'monsite.fr',
   :address => 'in-v3.mailjet.com',
   :port => 587,
   :authentication => :plain,
