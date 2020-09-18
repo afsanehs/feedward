6cb2d93467d0cd92485b111997ab0e45fc04855c
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-19 01:55:36 +0200

    Update notification accounts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,31 @@
 Rails.application.routes.draw do
   root to:'static_pages#landing'
 
+  # Devise routes
   devise_for :users
   devise_scope :user do
     # Fix problem of sign out button devise
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
   # Active admin
   ActiveAdmin.routes(self)
   devise_for :admin_users, {class_name: 'User'}.merge(ActiveAdmin::Devise.config)
 
-  
+  # priciple controller for model
   resources :users, only: [:show]
   resources :feedbacks, except: [:destroy]
   resources :notifications, only: [:index, :create, :update, :destroy]
   resources :appointments
   resources :companies, only: [:new, :create, :edit, :update]
   
-
+  # Custom urls user
+  resources :accounts, only: [:index, :create]
+  resources :request_companies, only: [:index, :create]
+  resources :request_users, only: [:show]
+  resources :spotifies, only: [:index]
+  resources :company_users, only: [:new, :create]
+  resources :notification_accounts, only: [:update, :destroy]
 
   
   # Static page
@@ -29,17 +37,9 @@ Rails.application.routes.draw do
   get '/landing_employees', to: 'static_pages#landing_employees',as: 'landing_employees'
   
 
-  # Custom urls user
-  resources :accounts, only: [:index, :create]
-  resources :request_companies, only: [:index, :create]
-  resources :request_users, only: [:show]
-  resources :spotifies, only: [:index]
-  resources :company_users, only: [:new, :create]
+  
 
 
   get '/users/:id/feedbacks', to: 'feedbacks#user_feedbacks', as: 'users_feedbacks'
-  patch 'validate_account/:id', to: "notifications#validate_account", as: 'validate_account'
-  delete 'refuse_account/:id', to: "notifications#refuse_account", as: 'refuse_account'
-
 
 end
